af1e8fbb18cf0d11b0e54dbe010c8cb87319db84
erency3 <74506083+erency3@users.noreply.github.com>
2020-11-18 14:25:58 +0300

    Add files via upload

---
+++ CareTaker.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class CareTaker {
+
+    private ArrayList<Memento> history;
+    private int currState = -1;
+
+    public CareTaker () {
+        this.history =new ArrayList<>();
+    }
+
+    public void addMemento (Memento m) {
+        this.history.add(m);
+        currState = this.history.size() -1;
+    }
+
+    public Memento getMemento (int index) {
+        return history.get(index);
+    }
+
+    public Memento undo() {
+        System.out.println("Undoing..");
+        if (currState<=0){
+            currState = 0;
+            return getMemento(0);
+        }
+
+        currState--;
+        return getMemento(currState);
+    }
+
+    public Memento redo (){
+        System.out.println("Redoing..");
+        if (currState>= history.size()-1){
+            currState = history.size()-1;
+            return getMemento(currState);
+        }
+        currState++;
+        return getMemento(currState);
+    }
+}
